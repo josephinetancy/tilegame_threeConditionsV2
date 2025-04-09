36984f8f19b4ffea59a65c624cbf2e20b85fc97a
josephinetancy <99933363+josephinetancy@users.noreply.github.com>
2025-04-09 11:30:50 -0700

    updated $$

--- jsFiles/streakgame.js
+++ jsFiles/streakgame.js
@@ -163,7 +163,7 @@ let avatarResponse = '#2669ee';
                 `<div class='parent'>
                 <p> For both games, you'll earn points. </p>
                 <p>Every 5 points is worth 1 cent in bonus money. </p>
-                You'll keep all the bonus money you earn on top of the $3.60 you earn for your participation. </p>
+                You'll keep all the bonus money you earn on top of the $2.80 you earn for your participation. </p>
                 <p>To maximize your earnings, earn as many points as possible!</p>
                 </div>`,
                 ],
@@ -1090,7 +1090,7 @@ function getCorrectAnswers(randomAssignment) {
     The risks associated with this study are not anticipated to be beyond those involved in normal, daily computer use. There are no foreseeable psychological risks and benefits beyond those involved in normal, daily life. The benefits which may reasonably be expected to result from this study are none. We cannot and do not guarantee or promise that you will receive any benefits from this study.
     
     <p><b>Payment</b><br>
-    You will receive $3.60 payment for your participation. You'll receive an additional bonus based on your performance in the experiment.  
+    You will receive $2.80 payment for your participation. You'll receive an additional bonus based on your performance in the experiment.  
 
      <p><b>Payment</b><br>
     If you have read this form and have decided to participate in this project, please understand your participation is voluntary and you have the right to withdraw your consent or discontinue participation at any time without penalty or loss of benefits to which you are otherwise entitled. The alternative is not to participate. You have the right to refuse to answer particular questions. The results of this research study may be presented at scientific or professional meetings or published in scientific journals. Your individual privacy will be maintained in all published and written data resulting from the study.
