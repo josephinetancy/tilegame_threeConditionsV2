737bbe678165a7cbb06b8b7d63b1d6c88a55c39b
josephinetancy <99933363+josephinetancy@users.noreply.github.com>
2025-04-08 22:44:29 -0700

    update no of trials

--- jsFiles/streakgame.js
+++ jsFiles/streakgame.js
@@ -1277,7 +1277,7 @@ variables for plugins
 ///
 */
 
-let noOfTrials = 2; //so 60 in total
+let noOfTrials = 15; //so 60 in total
 
 ///fake participant's activation time for WL and LL trials, that far exceeds trial duration
 let partner_rtL = 20000; //for when partner "loses".
